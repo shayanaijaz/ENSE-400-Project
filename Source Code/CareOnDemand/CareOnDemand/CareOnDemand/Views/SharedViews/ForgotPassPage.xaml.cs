9b1e39190384798cc242b714893bdbb695559089
Nicolas Achter <nicolas_achter@hotmail.com>
2020-01-13 12:37:09 -0600

    Added source code project

---
+++ Source Code/CareOnDemand/CareOnDemand/CareOnDemand/Views/SharedViews/ForgotPassPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CareOnDemand.Views.SharedViews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ForgotPassPage : ContentPage
+    {
+        public ForgotPassPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
