23c00b94d99eba277eb8a64977933712de6e6dfa
SchmidtyNick <32582247+SchmidtyNick@users.noreply.github.com>
2020-01-21 12:52:05 -0600

    Added Customer View pages
    Created Files for Customer Views regarding placing home care orders

---
+++ Source Code/CareOnDemand/CareOnDemand/CareOnDemand/Views/CustomerViews/ServiceSelection.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CareOnDemand.Views.CustomerViews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ServiceSelection : ContentPage
+    {
+        public ServiceSelection()
+        {
+            InitializeComponent();
+        }
+    }
+}
