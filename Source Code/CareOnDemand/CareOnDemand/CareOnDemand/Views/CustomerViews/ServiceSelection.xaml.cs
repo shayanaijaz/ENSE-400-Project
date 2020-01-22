92523b0532f267118e9b561c0fcb2fe947f7f204
SchmidtyNick <32582247+SchmidtyNick@users.noreply.github.com>
2020-01-22 17:12:43 -0600

    Service Selector Picke
    Added "picker" list for Service Selection & defined the "Get item" function in the .cs page as well as populated the picker

--- Source Code/CareOnDemand/CareOnDemand/CareOnDemand/Views/CustomerViews/ServiceSelection.xaml.cs
+++ Source Code/CareOnDemand/CareOnDemand/CareOnDemand/Views/CustomerViews/ServiceSelection.xaml.cs
@@ -15,6 +15,19 @@ namespace CareOnDemand.Views.CustomerViews
         public ServiceSelection()
         {
             InitializeComponent();
+            ServiceSelector.Items.Add("T1"); //Populate picker
+            ServiceSelector.Items.Add("T2"); //Populate picker
+            ServiceSelector.Items.Add("T3"); //Populate picker
+            ServiceSelector.Items.Add("T4"); //Populate picker
+            ServiceSelector.Items.Add("T5"); //Populate picker
+            ServiceSelector.Items.Add("T6"); //Populate picker
+        }
+
+        //Used to get the value of a selected service in serviceSelector Picker
+        private void ServiceSelector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           var service = ServiceSelector.Items[ServiceSelector.SelectedIndex];
+           DisplayAlert(service, "Was selected", "Ok"); //Display test to confirm picker is working
         }
     }
 }
