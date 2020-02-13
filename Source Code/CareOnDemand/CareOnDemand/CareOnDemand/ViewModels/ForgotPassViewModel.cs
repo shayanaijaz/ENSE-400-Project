df36de574dd97595e4b78bee1b2a0fbdb4d29afb
Shayan Khan <shayanaijaz@gmail.com>
2020-02-13 17:48:07 -0600

    preliminary forgot password implementation, removed change pass success page

--- Source Code/CareOnDemand/CareOnDemand/CareOnDemand/ViewModels/ForgotPassViewModel.cs
+++ Source Code/CareOnDemand/CareOnDemand/CareOnDemand/ViewModels/ForgotPassViewModel.cs
@@ -6,14 +6,37 @@ using CareOnDemand.Views.CustomerViews;
 using System.Windows.Input;
 using System.Threading.Tasks;
 using CareOnDemand.Models;
+using CareOnDemand.Data;
 
 namespace CareOnDemand.ViewModels
 {
-    public class ForgotPassViewModel
+    public class ForgotPassViewModel    : BaseViewModel
     {
-    }
-           // private void Button_Clicked(object sender, EventArgs e)
-        //{
+        private String email;
+        public ForgotPassViewModel()
+        {
+            ForgotPasswordCommand = new Command(ForgotPassword);
+        }
 
-        //}
+        public String Email
+        {
+            get => email;
+            set
+            {
+                email = value;
+            }
+        }
+
+        public Command ForgotPasswordCommand { private set; get; }
+
+        async void ForgotPassword()
+        {
+            ForgotPasswordService forgotPasswordService = new ForgotPasswordService(email);
+
+            await forgotPasswordService.ForgotPassowrd();
+
+            await Application.Current.MainPage.DisplayAlert("Success", "Password reset request successful. Please check your email for verification code.", "OK");
+        }
+    }
+
 }
