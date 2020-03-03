d116ae7961ada5da1e1da4f9e4f1ea4384d9073d
Shayan Khan <shayanaijaz@gmail.com>
2020-03-02 22:11:20 -0600

    improved service selection list page

--- Source Code/CareOnDemand/CareOnDemand/CareOnDemand/Views/CustomerViews/ServiceSelectedDetails.xaml.cs
+++ Source Code/CareOnDemand/CareOnDemand/CareOnDemand/Views/CustomerViews/ServiceSelectedDetails.xaml.cs
@@ -13,12 +13,26 @@ namespace CareOnDemand.Views.CustomerViews
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ServiceSelectedDetails : ContentPage
     {
+        ViewCell lastCell;
         public ServiceSelectedDetails()
         {
             InitializeComponent();
 
             BindingContext = new ServiceSelectedDetailsViewModel();
 
+        }
+
+        private void ViewCellTapped(object sender, System.EventArgs e)
+        {
+            if (lastCell != null)
+                lastCell.View.BackgroundColor = Color.Transparent;
+
+            var viewCell = (ViewCell)sender;
+            if (viewCell.View != null)
+            {
+                viewCell.View.BackgroundColor = Color.FromHex("#a5d5ed");
+                lastCell = viewCell;
+            }
         }
     }
 }
