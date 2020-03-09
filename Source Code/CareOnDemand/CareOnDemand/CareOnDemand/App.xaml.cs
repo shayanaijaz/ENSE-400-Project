89908750bbc703016dbbb78d16e9a0a03b2709f8
NLendvoy <n.a.lendvoy@gmail.com>
2020-03-09 14:34:01 -0600

    Basic Admin Flow Pages updated/created

--- Source Code/CareOnDemand/CareOnDemand/CareOnDemand/App.xaml.cs
+++ Source Code/CareOnDemand/CareOnDemand/CareOnDemand/App.xaml.cs
@@ -24,7 +24,7 @@ namespace CareOnDemand
 
             if (!isLoggedIn)
             {
-                MainPage = new NavigationPage(new LoginPage());
+                MainPage = new NavigationPage(new ViewNewOrder());
             }
             else if (isLoggedIn)
             {
