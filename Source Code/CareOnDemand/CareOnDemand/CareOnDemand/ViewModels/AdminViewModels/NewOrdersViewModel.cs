26a149983d7c54362b7d5520371b5c8f5844a0b2
Shayan Khan <shayanaijaz@gmail.com>
2020-03-16 18:33:57 -0600

    added folders to view models, binding for additional instructions

---
+++ Source Code/CareOnDemand/CareOnDemand/CareOnDemand/ViewModels/AdminViewModels/NewOrdersViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareOnDemand.ViewModels.AdminViewModels
+{
+    public class NewOrdersViewModel : BaseViewModel
+    {
+
+    }
+}
