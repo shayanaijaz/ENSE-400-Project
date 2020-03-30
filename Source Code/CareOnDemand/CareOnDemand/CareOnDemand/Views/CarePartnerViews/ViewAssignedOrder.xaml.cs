6ee5965e13994b80fad8521a11f96d0407440ec2
Shayan Khan <shayanaijaz@gmail.com>
2020-03-30 17:21:53 -0600

    fixed a boo boo i made

--- Source Code/CareOnDemand/CareOnDemand/CareOnDemand/Views/CarePartnerViews/ViewAssignedOrder.xaml.cs
+++ Source Code/CareOnDemand/CareOnDemand/CareOnDemand/Views/CarePartnerViews/ViewAssignedOrder.xaml.cs
@@ -10,9 +10,9 @@ using Xamarin.Forms.Xaml;
 namespace CareOnDemand.Views.CarePartnerViews
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class CurrentOrder : ContentPage
+    public partial class ViewAssignedOrder : ContentPage
     {
-        public CurrentOrder()
+        public ViewAssignedOrder()
         {
             InitializeComponent();
         }
